e4e0db26907b9fbdede53a5d0b165441bc0902cf
Michael Disko <disko@art-lemon.com>
2017-02-12 17:41:53 +0200

    change js

--- dist/js/zellion.tabs.js
+++ dist/js/zellion.tabs.js
@@ -28,13 +28,11 @@
       var hashLink = window.location.hash;
       var hashLinkSlice = hashLink.slice(1);
       if ((window.location.hash !== '' && settings.hash === true) && ($this.find('.tab__link[href=' + hashLink + ']').length == 1)) {
-        $this
-          .find('.tab__link[href=' + hashLink + ']').closest('.tab__item').addClass('active')
-          .closest('.tab').find('#' + hashLinkSlice + '').addClass('block');
+        $this.find('.tab__link[href=' + hashLink + ']').closest('.tab__item').addClass('active');
+        $this.closest('.tab').find('#' + hashLinkSlice + '').addClass('block');
       } else {
-        $this
-          .find('.tab__item').eq(settings.defaultTab).addClass('active')
-          .closest('.tab').find('.tab__content').eq(settings.defaultTab).addClass('block');
+        $this.find('.tab__item').eq(settings.defaultTab).addClass('active');
+        $this.find('.tab__content').eq(settings.defaultTab).addClass('block');
       }
 
       function tabInit() {
@@ -49,19 +47,18 @@
             settings.beforeChange()
             $(this).closest('.tab__item').addClass('active').siblings().removeClass('active');
             if (settings.animation == 'fade') {
-              $('.tab__content').siblings().stop().fadeOut(0);
-              $('.tab__content[id=' + tabLinkSlice + ']').stop().fadeIn(settings.speed);
+              $(this).closest('.tab').find('.tab__content').siblings().stop().fadeOut(0);
+              $(this).closest('.tab').find('.tab__content[id=' + tabLinkSlice + ']').stop().fadeIn(settings.speed);
             } else if (settings.animation == 'slide') {
-              $('.tab__content').siblings().stop().slideUp(0);
-              $('.tab__content[id=' + tabLinkSlice + ']').stop().slideDown(settings.speed);
+              $(this).closest('.tab').find('.tab__content').siblings().stop().slideUp(0);
+              $(this).closest('.tab').find('.tab__content[id=' + tabLinkSlice + ']').stop().slideDown(settings.speed);
             } else if (settings.animation == 'slideToggle') {
-              $('.tab__content').siblings().stop().slideUp(settings.speed);
-              $('.tab__content[id=' + tabLinkSlice + ']').stop().slideDown(settings.speed);
+              $(this).closest('.tab').find('.tab__content').siblings().stop().slideUp(settings.speed);
+              $(this).closest('.tab').find('.tab__content[id=' + tabLinkSlice + ']').stop().slideDown(settings.speed);
             } else if (settings.animation == 'show') {
-              $('.tab__content').siblings().stop().hide();
-              $('.tab__content[id=' + tabLinkSlice + ']').stop().show();
+              $(this).closest('.tab').find('.tab__content').siblings().stop().hide();
+              $(this).closest('.tab').find('.tab__content[id=' + tabLinkSlice + ']').stop().show();
             }
-
             setTimeout(function() {
               cb = settings.afterChange
               if (cb && typeof cb === 'function') {
