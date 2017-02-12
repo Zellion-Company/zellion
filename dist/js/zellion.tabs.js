c0de94ead7c4f07e44cae387ae14a9259b6b8bc6
Michael Disko <disko@art-lemon.com>
2017-02-12 16:09:25 +0200

    change js

--- dist/js/zellion.tabs.js
+++ dist/js/zellion.tabs.js
@@ -27,9 +27,9 @@
       var $this = $(this);
       var hashLink = window.location.hash;
       var hashLinkSlice = hashLink.slice(1);
-      if ((window.location.hash !== '' && settings.hash === true) && ($this.find(`.tab__link[href='${hashLink}']`).length == 1)) {
+      if ((window.location.hash !== '' && settings.hash === true) && ($this.find('.tab__link[href=' + hashLink + ']').length == 1)) {
         $this
-          .find(`.tab__link[href='${hashLink}']`).closest('.tab__item').addClass('active')
+          .find('.tab__link[href=' + hashLink + ']').closest('.tab__item').addClass('active')
           .closest('.tab').find('#' + hashLinkSlice + '').addClass('block');
       } else {
         $this
@@ -38,13 +38,13 @@
       }
 
       function tabInit() {
-        $('.tab__link').on('click', function(e, cb) {
-          e.preventDefault();
+        $('.tab__link').on('click', function(event, cb) {
+          event.preventDefault();
           var tabLink = $(this).attr('href');
           var tabLinkSlice = tabLink.slice(1);
           if (settings.hash === true) {
-  window.location.hash = tabLink
-}
+            window.location.hash = tabLink
+          }
           if (!($(this).closest('.tab__item').hasClass('active'))) {
             settings.beforeChange()
             $(this).closest('.tab__item').addClass('active').siblings().removeClass('active');
@@ -59,7 +59,7 @@
               $('.tab__content[id=' + tabLinkSlice + ']').stop().slideDown(settings.speed);
             } else if (settings.animation == 'show') {
               $('.tab__content').siblings().stop().hide();
-              $('.tab__content[id=' + tabLinkSlice + ']').stop().show()
+              $('.tab__content[id=' + tabLinkSlice + ']').stop().show();
             }
 
             setTimeout(function() {
